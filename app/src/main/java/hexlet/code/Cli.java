4daf1409a7e5a51cb235bf97ee20de9fc1353df6
Lukin <maximlm93@gmial.com>
2024-09-21 19:39:43 +0500

    fixed Cli

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Cli {
     public static void greeting() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("May I have your name? ");
+        System.out.print("\nWelcome to the Brain Games!"
+                        + "\nMay I have your name? ");
         var playerName = scanner.nextLine();
         System.out.println("Hello, " + playerName);
     }
