c0fb0331cae24070691afa41e7e65fa564fb85ee
Lukin <maximlm93@gmial.com>
2024-09-23 12:38:56 +0500

    update logic correctAnswer method

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -5,8 +5,6 @@ import hexlet.code.Util;
 
 public class Gcd {
 
-    private static int number1;
-    private static int number2;
     private static final String GAME_RULE = "Find the greatest common divisor of given numbers.";
 
     public static void gameStart() {
@@ -21,25 +19,14 @@ public class Gcd {
         return questionsAndAnswers;
     }
 
-    private static String correctAnswer() {
-        if (number1 == 0) {
-            return String.valueOf(number2);
-        }
-
-        while (number2 != 0) {
-            if (number1 > number2) {
-                number1 = number1 - number2;
-            } else {
-                number2 = number2 - number1;
-            }
-        }
-        return String.valueOf(number1);
+    private static String correctAnswer(int number1, int number2) {
+        return number2 == 0 ? String.valueOf(number1) : correctAnswer(number2, number1 % number2);
     }
 
     private static void generateRoundData(String[] oneRound) {
-        number1 = Util.generateRandomNumber(Engine.BOUND_TO_GENERATE_NUMBERS);
-        number2 = Util.generateRandomNumber(Engine.BOUND_TO_GENERATE_NUMBERS);
+        var number1 = Util.generateRandomNumber(Engine.BOUND_TO_GENERATE_NUMBERS);
+        var number2 = Util.generateRandomNumber(Engine.BOUND_TO_GENERATE_NUMBERS);
         oneRound[0] = (number1) + " " + (number2);
-        oneRound[1] = correctAnswer();
+        oneRound[1] = correctAnswer(number1, number2);
     }
 }
